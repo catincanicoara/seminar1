07bfa64643b918a73307b945f73889f87a8ac3d7
catincanicoara <nicoaracatinca@yahoo.com>
2022-03-03 09:22:47 +0200

    project created - hello world

--- src/ro/ase/cts/g1088/Main.java
+++ src/ro/ase/cts/g1088/Main.java
@@ -1,7 +1,40 @@
 package ro.ase.cts.g1088;
 
+import java.util.Scanner;
+
+class ExceptieAllower  extends  Exception{
+    @Override
+    public String getMessage(){
+        return "Nu au fost introduse litere mari";
+    }
+}
+
+class ExceptieAllowerSmallerCase extends Exception{
+    public String getMessage(){
+        return "Nu au fost introduse litere mici";
+    }
+}
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Testare");
+    public static void main(String[] args){
+        System.out.println("Tastati o propozitie care sa contina litere mari si mici: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti propozitia: ");
+        String str = scanner.nextLine();
+
+        try {
+
+            if(str.toLowerCase()==str)
+            {
+                throw new ExceptieAllower();
+            }
+            if(str.toUpperCase()==str){
+                throw new ExceptieAllowerSmallerCase();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
